d564de1857b1788d0f327b72f58f0760106aa3f3
intx4 <65897068+intx4@users.noreply.github.com>
2023-02-20 13:03:55 +0100

    added more reliant context generatio by allowing different level of tollerance over maxbinsize (useful for cases when logN is small or size is large)

--- settings/pirContext.go
+++ settings/pirContext.go
@@ -48,40 +48,53 @@ func NewPirContext(Items int, Size int, N int, Dimentions int) (*PirContext, err
 	ctx := &PirContext{Items: Items, MaxBinSize: int(math.Floor((float64(TUsableBits*N) - math.Log2(float64(TUsableBits*N)) - float64(3*TUsableBits)) / float64(Size+8))), N: N} //- for padding and length, +8 is 1 byte for separator "|"
 	//compute key space https://link.springer.com/content/pdf/10.1007/3-540-49543-6_13.pdf
 	base := math.E
-	exp := 2.0
-	maxIter := 1000
-	for maxIter > 0 {
-		K := math.Pow(base, exp)
-		c := float64(Items) / (K * math.Log(K))
-		prob := nonlin.Problem{F: func(out, x []float64) {
-			out[0] = 1.0 + x[0]*(math.Log(c)-math.Log(x[0])+1.0) - c
-		}}
-		solver := utils.NewtonKrylov{
-			// Maximum number of Newton iterations
-			Maxiter: 1e9,
+	tollerance := 1.0
+	for tollerance < 10.0 {
+		maxIter := 1000
+		exp := 1.1
+		for maxIter > 0 {
+			K := math.Pow(base, exp)
+			c := float64(Items) / (K * math.Log(K))
+			//speculatively skip until c is reasonably small
+			if math.Ceil((c+1)*exp) >= tollerance*float64(ctx.MaxBinSize) {
+				maxIter--
+				exp += .01
+				continue
+			}
+			prob := nonlin.Problem{F: func(out, x []float64) {
+				out[0] = 1.0 + x[0]*(math.Log(c)-math.Log(x[0])+1.0) - c
+			}}
+			solver := utils.NewtonKrylov{
+				// Maximum number of Newton iterations
+				Maxiter: 1e12,
 
-			// Stepsize used to approximate jacobian with finite differences
-			StepSize: 1e-2,
+				// Stepsize used to approximate jacobian with finite differences
+				StepSize: 1e-3,
 
-			// Tolerance for the solution
-			Tol: 1e-7,
-		}
-		dc := 0.0
-		roots := solver.Solve(prob, []float64{c, math.Pow(c, 2)}).X
-		for _, root := range roots {
-			if root > c {
-				dc = root
+				// Tolerance for the solution
+				Tol: 1e-6,
+			}
+			dc := 0.0
+			roots := solver.Solve(prob, []float64{c, math.Pow(c, 2)}).X
+			for _, root := range roots {
+				if root > c {
+					dc = root
+					break
+				}
+			}
+			if math.Ceil((dc+1)*exp) < tollerance*float64(ctx.MaxBinSize) && dc != 0.0 {
+				ctx.ExpBinSize = int(math.Ceil((dc + 1) * exp))
+				ctx.K, ctx.Kd = RoundUpToDim(K, Dimentions)
+				ctx.Dim = Dimentions
 				break
 			}
+			maxIter--
+			exp += .01
 		}
-		if math.Ceil((dc+1)*math.Log(K)) < 1.1*float64(ctx.MaxBinSize) && dc != 0.0 {
-			ctx.ExpBinSize = int(math.Ceil((dc + 1) * math.Log(K)))
-			ctx.K, ctx.Kd = RoundUpToDim(K, Dimentions)
-			ctx.Dim = Dimentions
+		if ctx.K != 0 {
 			break
 		}
-		maxIter--
-		exp += .1
+		tollerance += 0.1
 	}
 	if ctx.K == 0 {
 		return nil, errors.New("Could not estimate probabilistic bin size or right dimention split, try to adjust the BFV parameters")
